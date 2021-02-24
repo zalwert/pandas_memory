ffafe187edefef107b3366094cd1a63f36dad538
Maciej, Zalwert <maciej.zalwert@gmail.com>
2021-02-24 17:12:26 +0100

    Add types memory size check

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import sys
+import decimal
 
 String = ['EUROPE', 'AFRICA', 'ASIA', 'AMERICA']
 Integer = 5
@@ -18,3 +20,20 @@ example_df['col_floats'] = pd.to_numeric(example_df['col_floats'], downcast='flo
 
 print(example_df.dtypes)
 print(example_df.memory_usage())
+
+# check dtypes memory size in python
+d = {
+    "int": 0,
+    "float": 0.0,
+    "dict": dict(),
+    "set": set(),
+    "tuple": tuple(),
+    "list": list(),
+    "str": "a",
+    "unicode": u"a",
+    "decimal": decimal.Decimal(0),
+    "object": object(),
+    "boolean": True
+}
+for k, v in sorted(d.items()):
+    print(k, sys.getsizeof(v))
